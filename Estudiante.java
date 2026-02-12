b60d118220a1f133c6af2149c0e813e0aa691773
AndresMj34 <luis.majeae@gmail.com>
2026-02-12 07:09:29 -0500

    Main

---
+++ Estudiante.java
@@ -0,0 +1,17 @@
+public class Estudiante{
+ String nombre;
+ String apellido;
+ int id;
+
+public Estudiante(String nombre, String apellido, int id) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.id = id;
+}
+
+
+
+
+
+
+}
